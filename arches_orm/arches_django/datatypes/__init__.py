3626610aa0fe7f32f821f83917979765cec6ebaa
Phil Weir <phil.weir@flaxandteal.co.uk>
2023-12-17 03:55:27 +0000

    tidy linting

--- arches_orm/arches_django/datatypes/__init__.py
+++ arches_orm/arches_django/datatypes/__init__.py
@@ -1,3 +1,17 @@
 from ._register import get_view_model_for_datatype
+from . import (
+    concepts,
+    semantic,
+    resource_instances,
+    string,
+    user
+)
 
-__all__ = ["get_view_model_for_datatype"]
+__all__ = [
+    "get_view_model_for_datatype",
+    "concepts",
+    "semantic",
+    "resource_instances",
+    "string",
+    "user"
+]
