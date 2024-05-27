9c98696723b186c30583d0ea7414d7cc751c5748
Phil Weir <phil.weir@flaxandteal.co.uk>
2024-05-27 21:25:19 +0100

    fix: try winding back arches-orm

--- arches_orm/arches_django/middleware.py
+++ arches_orm/arches_django/middleware.py
@@ -8,3 +8,12 @@ class ArchesORMContextMiddleware:
         with get_adapter().context(user=request.user):
             response = self.get_response(request)
         return response
+
+class ArchesORMContextFreeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        with get_adapter().context_free():
+            response = self.get_response(request)
+        return response
