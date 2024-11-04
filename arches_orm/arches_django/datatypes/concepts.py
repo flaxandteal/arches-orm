6c3c3fd9f1093649d6239da71c1406f9c56e756a
Phil Weir <phil.weir@flaxandteal.co.uk>
2024-11-04 13:23:59 +0200

    wip: hierarchical support passes tests and can show it outputs correct scope notes, etc.

--- arches_orm/arches_django/datatypes/concepts.py
+++ arches_orm/arches_django/datatypes/concepts.py
@@ -9,6 +9,7 @@ from arches_orm.view_models import (
     ConceptListValueViewModel,
     ConceptValueViewModel,
     EmptyConceptValueViewModel,
+    StaticConcept,
 )
 from arches_orm.collection import make_collection, CollectionEnum
 from ._register import REGISTER
@@ -19,8 +20,17 @@ def invalidate_collection(concept_id):
     if concept_id in _COLLECTIONS:
         del _COLLECTIONS[concept_id]
 
+def retrieve_concept(concept_id: uuid.UUID, language: str | None, datatype) -> StaticConcept:
+    concept = Concept().get(id=concept_id, include=["label"])
+    # TODO: export values, etc.
+    return StaticConcept(
+        id=concept.id,
+        values={},
+        source=None,
+        children=[]
+    )
+
 def retrieve_children(concept_id: uuid.UUID, language: str | None, datatype) -> list[ConceptValueViewModel]:
-    # RMV TEST
     concept = Concept().get(id=concept_id, include=["label"])
 
     try:
@@ -44,6 +54,7 @@ def retrieve_collection(collection_id: uuid.UUID, datatype=None) -> type[Enum]:
         return ConceptValueViewModel(
             id,
             lambda value_id: datatype.get_value(value_id),
+            retrieve_concept,
             collection_id if collection_id else None,
             (lambda _: retrieve_collection(collection_id, datatype=datatype) if collection_id else None),
             partial(retrieve_children, datatype=datatype)
@@ -118,6 +129,7 @@ def make_concept_value(value: uuid.UUID | None, collection_id: uuid.UUID | None,
     return ConceptValueViewModel(
         value,
         concept_value_cb,
+        retrieve_concept,
         collection_id,
         partial(retrieve_collection, datatype=datatype),
         partial(retrieve_children, datatype=datatype)
--- tests/static/test_static.py
+++ tests/static/test_static.py
@@ -31,6 +31,78 @@ def test_can_save_collection(arches_orm):
 @context_free
 def test_can_insert_into_concept_scheme(arches_orm):
     rdm = get_adapter().get_rdm()
+    nismr_value = rdm.get_concept("23be33d2-c1c2-479b-b37a-603b474ce9aa")
+    NISMR = rdm.concept_to_collection(nismr_value)
+    assert NISMR.Dow037
+    graph = concept_to_skos(nismr_value.concept, "localhost:8000")
+    found = False
+    for concept, _, _ in graph.triples((None, RDF.type, SKOS.Concept)):
+        for _, _, label in graph.triples((concept, SKOS.prefLabel, None)):
+            label = str(label)
+            if "DOW 037" in label and "ded50ba1-9051-4cef-9da4-eabca689b9a7" in label:
+                found = True
+                break
+        if found:
+            break
+    assert found
+
+@context_free
+def test_can_update_fish_concept(arches_orm):
+    rdm = get_adapter().get_rdm()
+    fish_value = rdm.get_concept("844b63f0-27e3-358d-9362-147379fc3420")
+    FISHMonuments = rdm.concept_to_collection(fish_value)
+    assert FISHMonuments.CistercianNunnery
+    graph = concept_to_skos(fish_value.concept, "localhost:8000")
+    rdm.save_concept(fish_value, "/tmp/concept.xml")
+    found = False
+
+    parent = None
+    for concept, _, _ in graph.triples((None, RDF.type, SKOS.Concept)):
+        for _, _, label in graph.triples((concept, SKOS.prefLabel, None)):
+            label = str(label)
+            if "Cistercian Nunnery" in label and "5c858c23-c51d-444c-ac8d-bdf60ee2fc6b" in label:
+                found = concept
+            if "Nunnery" in label and "485fa3b3-b098-4a48-92de-68de152cc450" in label:
+                parent = concept
+
+    assert found
+    assert parent
+
+    for _, _, label in graph.triples((found, SKOS.altLabel, None)):
+        if "Cistercian Abbey" in str(label):
+            break
+    else:
+        raise AssertionError("AltLabel not found")
+
+    for _, _, label in graph.triples((found, SKOS.related, None)):
+        if "eda3537a-2ecd-3dbd-a702-63dbc6f51905" in str(label):
+            break
+    else:
+        raise AssertionError("Relationship not found")
+
+    for _, _, label in graph.triples((found, SKOS.scopeNote, None)):
+        if "An abbey or a priory of Cistercian nuns." in str(label):
+            break
+    else:
+        raise AssertionError("Relationship not found")
+
+    for _, _, label in graph.triples((parent, SKOS.narrower, None)):
+        if "875c129b-d23f-35dc-ae57-15226b8c4a12" in str(label):
+            break
+    else:
+        raise AssertionError("Child relationship not found")
+
+def test_can_derive_a_new_collection():
+    rdm = get_adapter().get_rdm()
+    StatusEnum = rdm.get_collection("7849cd3c-3f0d-454d-aaea-db9164629641")
+    concept_1 = rdm.make_simple_concept("My Status", "Done")
+    StatusEnum = rdm.derive_collection("7849cd3c-3f0d-454d-aaea-db9164629641", include=[concept_1], exclude=[StatusEnum.BacklogDashSkeleton.value])
+    assert len(StatusEnum.__members__) == 4
+    assert StatusEnum.Done
+
+@context_free
+def test_can_replace_collection(arches_orm):
+    rdm = get_adapter().get_rdm()
     StatusEnum = rdm.get_collection("7849cd3c-3f0d-454d-aaea-db9164629641")
     concept_1 = rdm.make_simple_concept("My Status", "Done")
     StatusEnum = rdm.derive_collection("7849cd3c-3f0d-454d-aaea-db9164629641", include=[concept_1], exclude=[StatusEnum.BacklogDashSkeleton.value])
