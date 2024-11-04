b64b3517bf11e849a9605bf8efd6c200c4345ff9
Phil Weir <phil.weir@flaxandteal.co.uk>
2024-11-04 20:12:57 +0200

    fix: support exporting a single collection

--- arches_orm/static/adapter.py
+++ arches_orm/static/adapter.py
@@ -4,6 +4,7 @@ import logging
 from pathlib import Path
 from enum import Enum
 from uuid import UUID
+from typing import IO
 from arches_orm.adapter import Adapter
 from arches_orm.view_models.concepts import ConceptValueViewModel
 from .wrapper import _STATIC_STORE
@@ -15,6 +16,7 @@ from .datatypes.concepts import (
     retrieve_concept_value,
     save_concept,
     update_collections,
+    export_collection,
     build_collection
 )
 
@@ -31,6 +33,12 @@ class StaticAdapter(Adapter):
     key = "static"
     _collections_loaded = False
 
+    def _check_collections_loaded(self):
+        if not self._collections_loaded:
+            for concept_path in self.config["concept_paths"]:
+                load_concept_path(concept_path)
+                load_collection_path(concept_path)
+
     def get_wrapper(self):
         from ..wrapper import ResourceWrapper
 
@@ -39,24 +47,26 @@ class StaticAdapter(Adapter):
     def update_collections(self, concept: ConceptValueViewModel, source_file: Path) -> None:
         update_collections(concept, source_file, arches_url=self.config["arches_url"])
 
+    def export_collection(self, concept: ConceptValueViewModel, source_file: Path | IO) -> None:
+        export_collection(concept, source_file, arches_url=self.config["arches_url"])
+
     def save_concept(self, concept: ConceptValueViewModel, output_file: Path | None) -> None:
         save_concept(concept, output_file, arches_url=self.config["arches_url"])
 
     def retrieve_concept_value(self, concept_id: str | UUID) -> ConceptValueViewModel:
+        self._check_collections_loaded()
         return retrieve_concept_value(concept_id)
 
     def make_concept(self, concept_id: str | UUID, values: dict[UUID, tuple[str, str]], children: list[UUID] | None) -> ConceptValueViewModel:
         return make_concept(concept_id, values, children)
 
-    def derive_collection(self, collection_id: str | UUID, include: list[UUID] | None, exclude: list[UUID] | None, language: str | None=None) -> type[Enum]:
+    def derive_collection(self, collection_id: str | UUID, include: list[UUID] | None=None, exclude: list[UUID] | None=None, language: str | None=None) -> type[Enum]:
         """Note that include and exclude should be lists of concept, not value, IDs."""
+        self._check_collections_loaded()
         return build_collection(collection_id, include=include, exclude=exclude, language=language)
 
     def get_collection(self, collection_id: str | UUID) -> type[Enum]:
-        if not self._collections_loaded:
-            for concept_path in self.config["concept_paths"]:
-                load_concept_path(concept_path)
-                load_collection_path(concept_path)
+        self._check_collections_loaded()
         return retrieve_collection(collection_id)
 
     def load_from_id(self, resource_id, from_prefetch=None):
