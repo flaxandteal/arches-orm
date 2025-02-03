adbe83b545b009ff806ea26bf50aba6c8935537b
Phil Weir <phil.weir@flaxandteal.co.uk>
2025-02-03 21:57:27 +0000

    fix(view_models): remove individual parser classes from edtf

--- arches_orm/view_models/edtf.py
+++ arches_orm/view_models/edtf.py
@@ -7,47 +7,12 @@ from ._base import (
     ViewModel,
 )
 
-def _make_edtf(value: str, **config):
+def make_edtf(value: str, **config):
     # TODO: use the config!
     try:
         edtf = parse_edtf(value)
     except EDTFParseException:
         edtf = parse_edtf(text_to_edtf(value))
-    if isinstance(edtf, Date):
-        return ExtendedDateViewModel(edtf)
-    elif isinstance(edtf, DateAndTime):
-        return ExtendedDateAndTimeViewModel(edtf)
-    elif isinstance(edtf, Interval):
-        return ExtendedIntervalViewModel(edtf)
-    elif isinstance(edtf, UncertainOrApproximate):
-        return ExtendedUncertainOrApproximateViewModel(edtf)
-    raise RuntimeError(f"Unrecognised EDTF class {type(edtf)} for {edtf}")
-
-# TODO: possibly slow, not very clean.
-def _add_view_model_class(obj, cls):
-    obj.__class__ = cls.__class__(f"Extended{cls.__name__}ViewModel", (cls, ViewModel), {})
-    return obj
-
-class ExtendedDateAndTimeViewModel(DateAndTime, ViewModel):
-    def __new__(cls, value: str | DateAndTime, **config):
-        if isinstance(value, str):
-            return _make_edtf(value, **config)
-        return _add_view_model_class(value, DateAndTime)
-
-class ExtendedDateViewModel(Date, ViewModel):
-    def __new__(cls, value: str | Date, **config):
-        if isinstance(value, str):
-            return _make_edtf(value, **config)
-        return _add_view_model_class(value, Date)
-
-class ExtendedIntervalViewModel(Interval, ViewModel):
-    def __new__(cls, value: str | Interval, **config):
-        if isinstance(value, str):
-            return _make_edtf(value, **config)
-        return _add_view_model_class(value, Interval)
-
-class ExtendedUncertainOrApproximateViewModel(UncertainOrApproximate, ViewModel):
-    def __new__(cls, value: str | Interval, **config):
-        if isinstance(value, str):
-            return _make_edtf(value, **config)
-        return _add_view_model_class(value, UncertainOrApproximate)
+    # TODO: possibly slow, not very clean.
+    edtf.__class__ = edtf.__class__.__class__(f"Extended{edtf.__class__.__name__}ViewModel", (edtf.__class__, ViewModel), {})
+    return edtf
