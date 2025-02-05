cf5549fc2428726f328a8b431d47f738f13ee449
aidan24 <aidan@galviadigital.com>
2025-02-05 14:48:11 +0000

    feat(#36): added domains to its own file

--- arches_orm/arches_django/datatypes/concepts.py
+++ arches_orm/arches_django/datatypes/concepts.py
@@ -140,3 +140,169 @@ def make_concept_value(value: uuid.UUID | None, collection_id: uuid.UUID | None,
 @concept_value.as_tile_data
 def cv_as_tile_data(concept_value):
     return None if isinstance(concept_value, EmptyConceptValueViewModel) else str(concept_value._concept_value_id)
+<<<<<<< HEAD
+||||||| parent of ebd21e0 (feat(#36): added domains to its own file)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def make_domain_value(value: uuid.UUID | None, domain_options: DomainOptions | None, datatype: str):
+
+    if value is None or isinstance(value, EmptyConceptValueViewModel):
+        if domain_options:
+            return EmptyConceptValueViewModel(
+                value,
+
+                partial(retrieve_collection, datatype=datatype)
+            )
+        return None
+
+    return DomainValueViewModel(
+        value,
+        domain_options,
+        datatype
+    )
+
+@REGISTER("domain-value")
+def domain_value(tile, node, value: uuid.UUID | None, _, __, ___, datatype) -> DomainValueViewModel:
+    if value is None:
+        value = tile.data.get(str(node.nodeid), []) or []
+
+    domain_options: DomainOptions | None = None
+
+    if node and node.config:
+        domain_options = node.config.get("options")
+
+    return make_domain_value(value, domain_options, datatype)
+
+@REGISTER("domain-value-list")
+def domain_value_list(tile, node, value: List[uuid.UUID] | None, _, __, ___, datatype):
+    # print('TILE |  ', tile)
+    # print('node |  ', node)
+    # print('value |  ', value)
+    # print('_ |  ', _)
+    # print('__ |  ', __)
+    # print('__ |  ', __)
+    # print('___ |  ', ___)
+    # print('datatype |  ', datatype)
+
+    # * We check if the value is set, if not we reterieve the value from a tile
+    if value is None or not value:
+        value = tile.data.get(str(node.nodeid), []) or []
+
+    domain_options: DomainOptions | None = None
+
+    def make_domain_value(value):
+        return REGISTER.make(tile, node, value=value, datatype="domain-value")[0]
+
+    if node and node.config:
+        domain_options = node.config.get("options")
+
+    DomainListValueViewModel(value, make_domain_value)
+
+@domain_value.as_tile_data
+def dv_as_tile_data(domain_value):
+    return domain_value.value
+
+@domain_value_list.as_tile_data
+def dvl_as_tile_data(domain_value_list):
+    print('dvl_as_tile_data | ', domain_value_list)
+
+    data = [dv_as_tile_data(domain_value) for domain_value in domain_value_list]
+
+    print('dvl_as_tile_data  data | ', data)
+
+
+    return data
+=======
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def make_domain_value(value: uuid.UUID | None, domain_options: DomainOptions | None, datatype: str):
+
+    if value is None or isinstance(value, EmptyConceptValueViewModel):
+        if domain_options:
+            return EmptyConceptValueViewModel(
+                value,
+
+                partial(retrieve_collection, datatype=datatype)
+            )
+        return None
+
+    return DomainValueViewModel(
+        value,
+        domain_options,
+        datatype
+    )
+
+@REGISTER("domain-value")
+def domain_value(tile, node, value: uuid.UUID | None, _, __, ___, datatype) -> DomainValueViewModel:
+    if value is None:
+        value = tile.data.get(str(node.nodeid), []) or []
+
+    domain_options: DomainOptions | None = None
+
+    if node and node.config:
+        domain_options = node.config.get("options")
+
+    return make_domain_value(value, domain_options, datatype)
+
+@domain_value.as_tile_data
+def dv_as_tile_data(domain_value):
+    return domain_value.value
+
+@REGISTER("domain-value-list")
+def domain_value_list(tile, node, value: List[uuid.UUID] | None, _, __, ___, datatype):
+    # print('TILE |  ', tile)
+    # print('node |  ', node)
+    # print('value |  ', value)
+    # print('_ |  ', _)
+    # print('__ |  ', __)
+    # print('__ |  ', __)
+    # print('___ |  ', ___)
+    # print('datatype |  ', datatype)
+
+    # * We check if the value is set, if not we reterieve the value from a tile
+    if value is None or not value:
+        value = tile.data.get(str(node.nodeid), []) or []
+
+    domain_options: DomainOptions | None = None
+
+    def make_domain_value(value):
+        return REGISTER.make(tile, node, value=value, datatype="domain-value")[0]
+
+    if node and node.config:
+        domain_options = node.config.get("options")
+
+    return DomainListValueViewModel(value, make_domain_value)
+
+@domain_value_list.as_tile_data
+def dl_as_tile_data(domain_value_list):
+    print('dvl_as_tile_data | ', domain_value_list)
+    data = [dv_as_tile_data(domain_value) for domain_value in domain_value_list]  # Should be a list, not a set
+    print('dvl_as_tile_data  data | ', data)
+    return data
+>>>>>>> ebd21e0 (feat(#36): added domains to its own file)
