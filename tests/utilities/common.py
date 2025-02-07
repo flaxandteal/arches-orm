8192bd48aba83bc8f6d45f8596dd2cb42cffc74a
aidan24 <aidan@galviadigital.com>
2025-02-07 17:02:39 +0000

    feat(#49): started to work on structurer

--- tests/utilities/common.py
+++ tests/utilities/common.py
@@ -119,6 +119,7 @@ def get_nodes_by_key(seed_set: str, key: str) -> Dict[str, any]:
         return {node[key]: node for node in nodes}
     
 def _handle_value_string_view_model(length=10):
+    return 'TEST'
     import random
     import string
 
