e8c6c8c523f18756fb398a56cb7f3e4082ed8030
aidan24 <aidan@galviadigital.com>
2025-02-10 12:17:42 +0000

    feat(#49): added solid structure querying system

---
+++ arches_orm/arches_django/query_builder/extractions.py
@@ -0,0 +1,30 @@
+
+
+class QueryBuilderExtraction:
+    def extract_value_datatype_string_():
+        print('HERE!')
+
+
+
+        {
+            '2caeb5e6-7b44-11ea-bc86-f875a44e0e11': None, 
+            '2caeb5e7-7b44-11ea-a919-f875a44e0e11': None, 
+            '32933f62-9503-11ea-bd92-f875a44e0e11': None, 
+            '4110f747-1a44-11e9-96b7-000d3ab1e588': None, 
+            '57c0b810-9503-11ea-a66b-f875a44e0e11': None, 
+            '5f8ded26-7ef9-11ea-8e29-f875a44e0e11': {'en': {'value': 'TEST', 'direction': 'ltr'}}, 
+            '6da2f03a-7e55-11ea-a919-f875a44e0e11': None, 
+            '6da2f03b-7e55-11ea-8fe5-f875a44e0e11': None, 
+            '75ff592e-7ef9-11ea-ab5c-f875a44e0e11': None, 
+            '79753c62-9502-11ea-82fc-f875a44e0e11': None, 
+            '8d818030-9503-11ea-89f1-f875a44e0e11': None, 
+            '8f5caea4-942e-11ea-b354-f875a44e0e11': None, 
+            '96a39429-7e53-11ea-8aa9-f875a44e0e11': None, 
+            '96a3942a-7e53-11ea-8b5a-f875a44e0e11': None, 
+            'a6638409-7e56-11ea-9294-f875a44e0e11': None, 
+            'a663840a-7e56-11ea-9e90-f875a44e0e11': None, 
+            'a72819d2-9502-11ea-b41d-f875a44e0e11': None, 
+            'e4f782cd-7e56-11ea-b5a8-f875a44e0e11': None, 
+            'e4f782ce-7e56-11ea-b3e3-f875a44e0e11': None, 
+            'e90ed55e-9502-11ea-81f2-f875a44e0e11': None
+        }
--- arches_orm/arches_django/query_builder/sub_classes/selectors.py
+++ arches_orm/arches_django/query_builder/sub_classes/selectors.py
@@ -1,2 +1,32 @@
+from arches.app.models.models import TileModel
+
 class QueryBuilderSelectors:
-    print('HEY')
+    _instance_query_builder = None;
+    _wrapper_instance = None;
+    _queryset_tiles = None;
+
+    def __init__(self, instance_query_builder):
+        self._instance_query_builder = instance_query_builder;
+        self._wrapper_instance = instance_query_builder._parent_wrapper_instance;
+    
+    @property
+    def queryset_tiles(self):
+        if not self._queryset_tiles:
+            self._queryset_tiles = TileModel.objects;
+
+        return self._queryset_tiles;
+
+    @queryset_tiles.setter
+    def queryset_tiles(self, value):
+        self._queryset_tiles = value;
+
+    def get(self):
+        annotations = self._instance_query_builder._annotations;
+        filters = self._instance_query_builder._filters;
+ 
+        def callable_get_tiles(**defaultFilterTileAgrs):
+            self.queryset_tiles = self.queryset_tiles.annotate(**annotations)
+            self.queryset_tiles = self.queryset_tiles.filter(**filters, **defaultFilterTileAgrs)
+            return self.queryset_tiles.select_related('resourceinstance', 'nodegroup').iterator()
+
+        return self._instance_query_builder.create_wkri_with_datatype_values(callable_get_tiles=callable_get_tiles)
