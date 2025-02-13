abdaa02c53933daf387deb9e6342659c021bf55e
aidan24 <aidan@galviadigital.com>
2025-02-13 17:05:38 +0000

    chore(#49): started adding comments

--- arches_orm/arches_django/query_builder/expressions.py
+++ arches_orm/arches_django/query_builder/expressions.py
@@ -1,11 +1,20 @@
 from django.db.models import F, ExpressionWrapper, IntegerField
 from django.db.models import Func, F, ExpressionWrapper, IntegerField, CharField
+from typing import Dict, List
+from arches.app.models.models import Node
 
-def annotation_string_datatype(nodeid: str):
+def expression_string_datatype(nodeid: str):
     # ! Not sure how to apporoach this
     default_lang = 'en'
 
     return ExpressionWrapper(
         F(f'data__{nodeid}__{default_lang}__value'),
         output_field=CharField()
+    )
+
+# ! Doesn't have a datatype
+def expression_number_datatype(nodeid: str):
+    return ExpressionWrapper(
+        F(f'data__{nodeid}'),
+        output_field=IntegerField()
     )
--- arches_orm/arches_django/query_builder/sub_classes/modifiers.py
+++ arches_orm/arches_django/query_builder/sub_classes/modifiers.py
@@ -1,3 +1,8 @@
+from ..utilities import annotation_key
+from typing import List
+from arches_orm.arches_django.query_builder.query_builder import QueryBuilder
+from arches.app.models.models import ResourceXResource, Node, NodeGroup, Edge, TileModel
+
 class QueryBuilderModifier:
     _instance_query_builder = None;
     _wrapper_instance = None;
@@ -7,5 +12,28 @@ class QueryBuilderModifier:
         self._instance_query_builder = instance_query_builder;
         self._wrapper_instance = instance_query_builder._parent_wrapper_instance;
     
-    def order_by(self):
+    def order_by(self, *args: List[str]) -> QueryBuilder:
+        """
+        Method handles the appending of order by for future filtering within the Django query in the file filters.py
+
+        Returns:
+            QueryBuilder: Enables chainable functionality 
+        """
+        
+        nodes: List[Node] = self._wrapper_instance._node_objects_by_alias();
+
+        # * Loops through arguments
+        for index in range(len(args)):
+            node_alias: str = args[index];
+            node: Node = nodes.get(node_alias.replace('-', ''))
+
+            # * Sets annotation if the key hasn't been setup for annotation
+            self._instance_query_builder.set_annotation(
+                node_alias, 
+                node
+            )
+
+            # * Using annotation provided, then orders by the annotation with appending the annotation onto the orders by from our parent
+            self._instance_query_builder._order_by.append(annotation_key(node_alias))
+    
         return self._instance_query_builder
