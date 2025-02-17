e2f55951c20bbea23b6c9214cb185268bbf78cab
aidan24 <aidan@galviadigital.com>
2025-02-17 17:03:48 +0000

    chore(#57): tired to add testing

--- tests/arches_django/sub_tests/arches_django_query_builder/read/filters/where.py
+++ tests/arches_django/sub_tests/arches_django_query_builder/read/filters/where.py
@@ -1,8 +1,10 @@
 from tests.utilities.common import create_tile_from_model
-from tests.utilities.seeders.default.person import person_primary_reference_number_odd_even_seeder
+from tests.utilities.seeders.default.person import person_primary_reference_number_odd_even_seeder, person_primary_date_50_50_older_past_seeder
+from datetime import datetime
 
 def sub_test_filter_where(arches_orm):
     sub_test_filter_where_number_equal(arches_orm)
+    sub_test_filter_where_date_equal(arches_orm)
 
 def sub_test_filter_where_number_equal(arches_orm):
     Person = arches_orm.models.Person
@@ -13,3 +15,13 @@ def sub_test_filter_where_number_equal(arches_orm):
 
     for record in records:
         assert(record.system_reference_numbers.primaryreferencenumber.primary_reference_number == 1)
+
+def sub_test_filter_where_date_equal(arches_orm):
+    Person = arches_orm.models.Person
+    person_primary_date_50_50_older_past_seeder(Person, 10)
+
+    today_date = datetime.today().strftime("%Y-%m-%d")
+
+    records = Person.where(associated_actor_start_date__lt=today_date).get();
+    print(len(records))
+    assert(len(records) == 5)
