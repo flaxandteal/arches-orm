4c7ba2b3aa584adaeb4c7036dabe21837e4ba90b
aidan24 <aidan@galviadigital.com>
2025-03-03 16:11:47 +0000

    fix(#124): fixed cache keys on _permitted_nodegroups

--- arches_orm/arches_django/wrapper.py
+++ arches_orm/arches_django/wrapper.py
@@ -480,19 +480,20 @@ class ArchesDjangoResourceWrapper(SearchMixin, ResourceWrapper, proxy=True):
             return list(cls._nodegroup_objects())
 
         if (permitted_nodegroups := context.get("user_graphs", {}).get(cls._model_name)):
-            png = permitted_nodegroups
+            permitted_nodegroups = permitted_nodegroups
         else:
             user = context.get("user")
             if user is None:
                 return list(cls._nodegroup_objects())
 
             png = get_permitted_nodegroups(user)
-        permitted_nodegroups = [
-            key for key in cls._nodegroup_objects()
-            if key in png or str(key) in png
-        ] + [None]
+            permitted_nodegroups = [
+                key for key in cls._nodegroup_objects()
+                if key in png or str(key) in png
+            ] + [None]
+
         context.setdefault("user_graphs", {})
-        context["user_graphs"][str(cls)] = permitted_nodegroups
+        context["user_graphs"][cls._model_name] = permitted_nodegroups
         return permitted_nodegroups
 
     @classmethod
@@ -1290,6 +1291,8 @@ class ArchesDjangoResourceWrapper(SearchMixin, ResourceWrapper, proxy=True):
                 value.append(node_value)
             else:
                 value = node_value
+
+        print('RETURNING VALUE HERE : _make_pseudo_node_cls')
 
         return value
 
